97e17e0194d710d9b06a50a95ad6879c87ec026e
bikramgiri <asgbrg.github@gmail.com>
2025-05-11 15:54:44 +0545

    Setup app file

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-console.log("Hello, World!")
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1>')
+})
+
+app.get('/about', (req, res) => {   
+      res.send('About Page')
+})
 
 
+
+
+app.listen(3000, () => {
+    console.log('Project is running on port 3000')
+})
 
